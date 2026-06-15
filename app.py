c15937ab1951d25343d473d23d163f043d50a508
Kambucha-scientist <123koneva@gmail.com>
2026-06-15 04:21:14 +0300

    for pythonanywhere

--- app.py
+++ app.py
@@ -10,15 +10,16 @@ from models import db, User, Role, Book, Genre, Cover, Review, book_genre
 from forms import BookForm, ReviewForm, LoginForm
 
 # ---------- Инициализация ----------
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecretkeychangeinproduction'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+app.config['SECRET_KEY'] = 'Rz46NsamfMAJH8U1HU3UHxC2UnlAyZLHdoUJ748iSnt'
+app.config['SQLALCHEMY_DATABASE_URI'] =  f'sqlite:///{os.path.join(BASE_DIR, "instance", "library.db")}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'uploads')
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  
 
-# Создаём папку для загрузок, если её нет
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+app.config['UPLOAD_FOLDER'] = os.path.join(BASE_DIR, 'uploads')
 
 db.init_app(app)
 login_manager = LoginManager(app)
